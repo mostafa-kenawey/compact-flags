790ec1c015f8fca28d3cadc160af22ab8d087156
Mahmoud Said <mahmoud@modsaid.com>
2010-08-03 17:04:58 +0300

    supporting arbitrary flags store name and clarifying usage in the README

--- lib/compact_flags.rb
+++ lib/compact_flags.rb
@@ -1,26 +1,31 @@
 module CompactFlags
   
-  def compact_flags(*flags)
+  
+  def compact_flags(mapping)
+    name = mapping.keys.first
+    flags = mapping[name]
+    raise "No flags defined" if flags.blank?
     raise "Too many flags" if flags.size > 31
     
-    @@flags||={}
+    @@flags ||={}
     i=0
+
     flags.each do |flag|
       @@flags[flag] = 1 << i
       i += 1 
       
       define_method "#{flag}" do
-        return (@@flags[flag] & self.compact_flags_store) > 0
+        return (@@flags[flag] & self.send(name)) > 0
       end
       
       alias_method "#{flag}?", "#{flag}"
       
       define_method "#{flag}=" do |val|
-        self.compact_flags_store = val ? 
-                    self.compact_flags_store | @@flags[flag] :
-                    self.compact_flags_store ^ @@flags[flag]
+        self.send("#{name}=", (val ? 
+            self.send(name) | @@flags[flag] :
+            self.send(name) ^ @@flags[flag]))
       end
     end
-  end
   
+  end
 end
